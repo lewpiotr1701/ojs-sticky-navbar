6218f2352f48ab62c61d152cf3115edf4ac00537
Piotr <lewczykpiotr@gmail.com>
2023-11-02 21:06:41 +0100

    add sticky navbar and hamburger menu with animation

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('Working');
+const hamburgerBtn = document.querySelector('.hamburger')
+const navbarMenuLinks = document.querySelector('.navbar__menu ul')
+const navbarItems = document.querySelectorAll('.navbar__item')
+
+// Toggle Nav
+hamburgerBtn.addEventListener('click', () => {
+  hamburgerBtn.classList.toggle('hamburger--active')
+  navbarMenuLinks.classList.toggle('navbar__menu--active')
+
+  // Animate Links
+  navbarItems.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = ''
+    } else {
+      link.style.animation = `navbarItemsFade 0.5s ease forwards ${index / 6}s`
+    }
+  })
+})
